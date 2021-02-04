4ae7b357a9666aaa0b63dec8dad15dbf43cb0cb4
siddhantk232 <siddhantk232@gmail.com>
2021-02-04 13:49:47 +0530

    change: Idefinition is now a type
    interface is not required to define the Definition
    type. Changed it to just be a `type = Record<UnitString, UnitObject>`
    
    fix: correct error messages
    feat: one more if check in Converter.to()

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,22 @@
  * for volume to mass units:
  * value(val: number).from(unit: Unit).to(unit: Unit)
  *
+ * Some Formulae:
+ * density = mass / volume
+ * volume = mass / density
+ * mass = density * volume
+ * weight = mass * g (g = 9.8 or 10)
+ *
  */
 
 type UnitString = "g" | "mg" | "kg" | "ounce";
 
 type UnitObject = {
   name: {
-    abbr: UnitString,
-    singular: string,
-    plural: string
-  }
+    abbr: UnitString;
+    singular: string;
+    plural: string;
+  };
   base: UnitString;
   factor: number;
   bulkDensity?: number;
@@ -26,50 +32,44 @@ type ConvertedUnit = UnitObject & {
   value: number;
 };
 
-interface IDefinition {
-  base: UnitString;
-  units: Record<UnitString, UnitObject>;
-}
+type Definition = Record<UnitString, UnitObject>;
 
-const definition: IDefinition = {
-  base: "g" as UnitString,
-  units: {
-    kg: {
-      name: {
-        abbr: "kg",
-        singular: "Kilogram",
-        plural: "Kilograms"
-      },
-      base: "g",
-      factor: 1000,
+const definition: Definition = {
+  kg: {
+    name: {
+      abbr: "kg",
+      singular: "Kilogram",
+      plural: "Kilograms",
     },
-    g: {
-      name: {
-        abbr: "g",
-        singular: "Gram",
-        plural: "Grams"
-      },
-      base: "g",
-      factor: 1,
+    base: "g",
+    factor: 1000,
+  },
+  g: {
+    name: {
+      abbr: "g",
+      singular: "Gram",
+      plural: "Grams",
     },
-    mg: {
-      name: {
-        abbr: "mg",
-        singular: "Miligram",
-        plural: "MiliGrams"
-      },
-      base: "g",
-      factor: 1 / 1000,
+    base: "g",
+    factor: 1,
+  },
+  mg: {
+    name: {
+      abbr: "mg",
+      singular: "Miligram",
+      plural: "MiliGrams",
     },
-    ounce: {
-      name: {
-        abbr: "ounce",
-        singular: "Ounce",
-        plural: "Ounces"
-      },
-      base: "g",
-      factor: 28.3495,
+    base: "g",
+    factor: 1 / 1000,
+  },
+  ounce: {
+    name: {
+      abbr: "ounce",
+      singular: "Ounce",
+      plural: "Ounces",
     },
+    base: "g",
+    factor: 28.3495,
   },
 };
 
@@ -83,18 +83,18 @@ const throwUnsupportedUnitError = () => {
 
 class Converter {
   value: number;
-  definition: IDefinition;
+  definition: Definition;
   toUnit: ConvertedUnit | null;
   fromUnit: UnitObject | null;
 
-  constructor(value: number, definition: IDefinition) {
+  constructor(value: number, definition: Definition) {
     this.value = value;
     this.definition = definition;
   }
 
   to(unit: UnitString): ConvertedUnit | null {
     if (!this.fromUnit) {
-      throwUnsupportedUnitError();
+      throwInvalidUseError("you need to call from() before calling to()");
       return null;
     }
 
@@ -105,27 +105,32 @@ class Converter {
       return null;
     }
 
-    const result: ConvertedUnit = {
+    if (to.base !== this.fromUnit.base) {
+      throwInvalidUseError(
+        "base unit of from and to must be same. Please check your definitions"
+      );
+      return null;
+    }
+
+    return {
       ...to,
       // convert this.value to base unit then divide it by to.factor
       value: (this.value * this.fromUnit.factor) / to.factor,
     };
-
-    return result;
   }
 
   from(unit: UnitString) {
     if (this.toUnit)
       throwInvalidUseError("cannot call from() after calling to().");
-    this.fromUnit = this.getUnit(unit);
 
+    this.fromUnit = this.getUnit(unit);
     if (!this.fromUnit) throwUnsupportedUnitError();
 
     return this;
   }
 
   getUnit(unit: UnitString): UnitObject | null {
-    return this.definition.units[unit];
+    return this.definition[unit];
   }
 }
 
